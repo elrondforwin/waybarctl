ddfc80b5d180de18073cdc137475376873a0d5dd
elrond <oguzemrearal55@gmail.com>
2025-08-12 14:20:34 +0300

    Updated install script, no need for alias now.

--- install-waybarctl.sh
+++ install-waybarctl.sh
@@ -8,37 +8,12 @@ if [ ! -d "$installdir" ]; then
   echo "Successfully created installdir on $installdir"
 fi
 
-if [ -f /home/$USER/.local/bin/waybarctl.sh ]; then
-  rm /home/$USER/.local/bin/waybarctl.sh
+if [ -f /home/$USER/.local/bin/waybarctl ]; then
+  rm /home/$USER/.local/bin/waybarctl
   echo "Found existing waybarctl. Removing the old one."
 fi
 
-touch /home/$USER/.local/bin/waybarctl.sh
+touch /home/$USER/.local/bin/waybarctl
 echo "Installing waybarctl..."
-curl -fsSL https://raw.githubusercontent.com/elrondforwin/waybarctl/refs/heads/main/waybarctl.sh >>/home/$USER/.local/bin/waybarctl.sh
+curl -fsSL https://raw.githubusercontent.com/elrondforwin/waybarctl/refs/heads/main/waybarctl >>/home/$USER/.local/bin/waybarctl
 echo "Succesfully installed waybarctl."
-
-if [ -f /home/$USER/.local/bin/waybarctl.sh ]; then
-  if [ -f /bin/bash ]; then
-    if ! grep -Fxq "alias waybarctl='bash /home/$USER/.local/bin/waybarctl.sh'" /home/$USER/.bashrc; then
-      printf "\n%s\n" "alias waybarctl='bash /home/$USER/.local/bin/waybarctl.sh'" >>"/home/$USER/.bashrc"
-      echo "Succesfully installed BASH configration."
-    fi
-  fi
-
-  if [ -f /bin/zsh ]; then
-    if ! grep -Fxq "alias waybarctl='bash /home/$USER/.local/bin/waybarctl.sh'" /home/$USER/.zshrc; then
-      printf "\n%s\n" "alias waybarctl='bash /home/$USER/.local/bin/waybarctl.sh'" >>"/home/$USER/.zshrc"
-      echo "Succesfully installed ZSH configration."
-    fi
-  fi
-
-  if [ -f /bin/fish ]; then
-    if ! grep -Fxq "alias waybarctl='bash /home/$USER/.local/bin/waybarctl.sh'" /home/$USER/.config/fish/config.fish; then
-      printf "\n%s\n" "alias waybarctl='bash /home/$USER/.local/bin/waybarctl.sh'" >>"/home/$USER/.config/fish/config.fish"
-      echo "Succesfully installed fish configration."
-    fi
-  fi
-else
-  echo "Cannot find script on $installdir. Exiting."
-fi
